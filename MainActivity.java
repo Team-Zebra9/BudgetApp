360e730b876208876bcbccdc0c75650658effa3c
zade-softwareengineering <35471064+zade-softwareengineering@users.noreply.github.com>
2021-03-11 16:11:53 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tothemoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setBackgroundColor(Color.rgb(51,153,255));
+    }
+}
